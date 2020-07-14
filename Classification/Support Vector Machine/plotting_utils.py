713c448e4779a8503a24e12b0bf085dd5635f1b0
REDDY PRASAD <15084222+reddyprasade@users.noreply.github.com>
2020-07-14 10:36:34 +0530

    Add files via upload

---
+++ Classification/Support Vector Machine/plotting_utils.py
@@ -0,0 +1,140 @@
+# version 1.0
+
+import numpy
+import pandas as pd
+import seaborn as sn
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from matplotlib.colors import ListedColormap, BoundaryNorm
+from sklearn import neighbors
+import matplotlib.patches as mpatches
+import graphviz
+from sklearn.tree import export_graphviz
+import matplotlib.patches as mpatches
+
+def plot_feature_importances(clf, feature_names):
+    c_features = len(feature_names)
+    plt.barh(range(c_features), clf.feature_importances_)
+    plt.xlabel("Feature importance")
+    plt.ylabel("Feature name")
+    plt.yticks(numpy.arange(c_features), feature_names)
+
+def plot_labelled_scatter(X, y, class_labels):
+    num_labels = len(class_labels)
+
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+
+    marker_array = ['o', '^', '*']
+    color_array = ['#FFFF00', '#00AAFF', '#000000', '#FF00AA']
+    cmap_bold = ListedColormap(color_array)
+    bnorm = BoundaryNorm(numpy.arange(0, num_labels + 1, 1), ncolors=num_labels)
+    fig = plt.figure()
+
+    plt.scatter(X[:, 0], X[:, 1], s=65, c=y, cmap=cmap_bold, norm = bnorm, alpha = 0.40, edgecolor='black', lw = 1)
+
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+
+    h = []
+    for c in range(0, num_labels):
+        h.append(mpatches.Patch(color=color_array[c], label=class_labels[c]))
+    plt.legend(handles=h)
+
+    plt.show()
+
+
+def plot_class_regions_for_classifier_subplot(clf, X, y, X_test, y_test, title, subplot, target_names = None, plot_decision_regions = True):
+
+    numClasses = numpy.amax(y) + 1
+    color_list_light = ['#FFFFAA', '#EFEFEF', '#AAFFAA', '#AAAAFF']
+    color_list_bold = ['#EEEE00', '#000000', '#00CC00', '#0000CC']
+    cmap_light = ListedColormap(color_list_light[0:numClasses])
+    cmap_bold  = ListedColormap(color_list_bold[0:numClasses])
+
+    h = 0.03
+    k = 0.5
+    x_plot_adjust = 0.1
+    y_plot_adjust = 0.1
+    plot_symbol_size = 50
+
+    x_min = X[:, 0].min()
+    x_max = X[:, 0].max()
+    y_min = X[:, 1].min()
+    y_max = X[:, 1].max()
+    x2, y2 = numpy.meshgrid(numpy.arange(x_min-k, x_max+k, h), numpy.arange(y_min-k, y_max+k, h))
+
+    P = clf.predict(numpy.c_[x2.ravel(), y2.ravel()])
+    P = P.reshape(x2.shape)
+
+    if plot_decision_regions:
+        subplot.contourf(x2, y2, P, cmap=cmap_light, alpha = 0.8)
+
+    subplot.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold, s=plot_symbol_size, edgecolor = 'black')
+    subplot.set_xlim(x_min - x_plot_adjust, x_max + x_plot_adjust)
+    subplot.set_ylim(y_min - y_plot_adjust, y_max + y_plot_adjust)
+
+    if (X_test is not None):
+        subplot.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cmap_bold, s=plot_symbol_size, marker='^', edgecolor = 'black')
+        train_score = clf.score(X, y)
+        test_score  = clf.score(X_test, y_test)
+        title = title + "\nTrain score = {:.2f}, Test score = {:.2f}".format(train_score, test_score)
+
+    subplot.set_title(title)
+
+    if (target_names is not None):
+        legend_handles = []
+        for i in range(0, len(target_names)):
+            patch = mpatches.Patch(color=color_list_bold[i], label=target_names[i])
+            legend_handles.append(patch)
+        subplot.legend(loc=0, handles=legend_handles)
+
+
+def plot_class_regions_for_classifier(clf, X, y, X_test=None, y_test=None, title=None, target_names = None, plot_decision_regions = True):
+
+    numClasses = numpy.amax(y) + 1
+    color_list_light = ['#FFFFAA', '#EFEFEF', '#AAFFAA', '#AAAAFF']
+    color_list_bold = ['#EEEE00', '#000000', '#00CC00', '#0000CC']
+    cmap_light = ListedColormap(color_list_light[0:numClasses])
+    cmap_bold  = ListedColormap(color_list_bold[0:numClasses])
+
+    h = 0.03
+    k = 0.5
+    x_plot_adjust = 0.1
+    y_plot_adjust = 0.1
+    plot_symbol_size = 50
+
+    x_min = X[:, 0].min()
+    x_max = X[:, 0].max()
+    y_min = X[:, 1].min()
+    y_max = X[:, 1].max()
+    x2, y2 = numpy.meshgrid(numpy.arange(x_min-k, x_max+k, h), numpy.arange(y_min-k, y_max+k, h))
+
+    P = clf.predict(numpy.c_[x2.ravel(), y2.ravel()])
+    P = P.reshape(x2.shape)
+    fig=plt.figure(dpi=100)
+    if plot_decision_regions:
+        plt.contourf(x2, y2, P, cmap=cmap_light, alpha = 0.8)
+
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold, s=plot_symbol_size, edgecolor = 'black')
+    plt.xlim(x_min - x_plot_adjust, x_max + x_plot_adjust)
+    plt.ylim(y_min - y_plot_adjust, y_max + y_plot_adjust)
+
+    if (X_test is not None):
+        plt.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cmap_bold, s=plot_symbol_size, marker='^', edgecolor = 'black')
+        train_score = clf.score(X, y)
+        test_score  = clf.score(X_test, y_test)
+        title = title + "\nTrain score = {:.2f}, Test score = {:.2f}".format(train_score, test_score)
+
+    if (target_names is not None):
+        legend_handles = []
+        for i in range(0, len(target_names)):
+            patch = mpatches.Patch(color=color_list_bold[i], label=target_names[i])
+            legend_handles.append(patch)
+        plt.legend(loc=0, handles=legend_handles)
+
+    if (title is not None):
+        plt.title(title)
+    plt.show()
+    #fig.savefig("RBF.svg", format="svg")
+
